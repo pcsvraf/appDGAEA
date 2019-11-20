bde6dc1de22ba82c0da5d0ba1cf7a7a71320acdb
calidad <pcsvraf@gmail.com>
2019-11-20 13:58:05 -0400

    integracion firebase

---
+++ app/src/main/java/com/servicio/calidad/app/dgaeaapp/login.java
@@ -0,0 +1,18 @@
+package com.servicio.calidad.app.dgaeaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
